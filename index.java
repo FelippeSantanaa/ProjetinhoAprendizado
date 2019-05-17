ed35c882829ac93666cb9bb537287325a1e6194b
Felipe Santana <felipe.santana@c6bank.com>
2019-05-17 16:58:31 -0300

    first commit

---
+++ index.java
@@ -0,0 +1,21 @@
+[11:33, 27/3/2019] Felipe_santana: .
+[11:38, 27/3/2019] Felipe_santana: import java.util.Scanner();
+
+Scanner felipe = new Scanner(system.in);
+
+System.out.println("Digite um dia da semana 1-7");
+int diaSemana = felipe.nextInt();
+
+switch (diaSemana);
+case 'Domingo':
+case "Sabado":
+System.out.println("Final de Semana");;break;
+
+case "Segunda":
+case "Terca":
+case "Quarta":
+case "Quinta":
+case "Sexta":
+System.out.println("Dia util");break;
+
+default System.out.println("Entre com um dia da semana valido");
